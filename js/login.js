9c05edd4033d63791ac010094ca5dea38fb22b97
unknown <asadalgalib9@gmail.com>
2024-09-21 12:41:18 +0600

    first commit

---
+++ js/login.js
@@ -0,0 +1,14 @@
+// console.log("button");
+
+document.getElementById("btn-login").addEventListener('click',function(event){
+    event.preventDefault(); 
+    const phoneNumber = document.getElementById("phone-num").value;
+    const pinNumber = document.getElementById("pin-num").value;
+   
+    if(phoneNumber === '5' && pinNumber === '12345'){
+        window.location.href = '/home.html';
+    }
+    else{
+        alert('invalid phone number or pin');
+    }
+});                                                                                        
